56c047a5f1da3a918dfa8abd5d32963f5af3876a
danzelVash <109025620+danzelVash@users.noreply.github.com>
2022-09-01 23:20:27 +0300

    Add files via upload

---
+++ start_work.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def get_info(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        print(response)
+    except requests.exceptions.ConnectionError:
+        print('Check yor connection')
+
+
+def main():
+    ip = input('Enter IP: ')
+    get_info(ip=ip)
+
+
+if __name__ == '__main__':
+    main()
